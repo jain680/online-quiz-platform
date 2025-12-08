7ce8e9bf271fc24aad7fdeeb34c77fdc8848dd23
Bhavya jain <145316274+jain680@users.noreply.github.com>
2025-12-08 08:55:38 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://online-quiz-platform-3.onrender.com/api'
+  baseURL: process.env.REACT_APP_API_URL 
+    ? process.env.REACT_APP_API_URL
+    : "https://online-quiz-platform-3.onrender.com/api/"
 });
 
 api.interceptors.request.use(config => {
